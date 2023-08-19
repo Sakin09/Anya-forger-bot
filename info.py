db2ba0f8e75e6aab4020f4a125a238e711d1e52f
Saichar1 <138511826+Saichar1@users.noreply.github.com>
2023-08-19 18:53:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 # Bot images & videos
 PICS = (environ.get('PICS', 'https://telegra.ph/file/86b5fa8fd112f868c47b5.jpg https://telegra.ph/file/f3e0d751f3f41b395f71c.jpg https://telegra.ph/file/c30410854a63fbcb2c746.jpg https://telegra.ph/file/2a014df746758b93c3ca1.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/0593a3103ba1b9a5855bf.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/b6fe8e8e746039e216734.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/07e0044e08e38195854e2.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
 
